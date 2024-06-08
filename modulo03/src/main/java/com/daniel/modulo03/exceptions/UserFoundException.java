a26593b5419e7d8947d5696c4c4bbd7806ec72ea
danielfreitassc <danielfreitassc@gmail.com>
2024-06-08 18:40:52 -0300

    Recomeçando

--- modulo03/src/main/java/com/daniel/modulo03/exceptions/UserFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.daniel.modulo03.exceptions;
-
-public class UserFoundException extends RuntimeException {
-    public UserFoundException() {
-        super("Usuário já existe");
-    }
-    
-}
