fdb08fe08ba10b43a093cc63180f14ca42241af9
DanielFreitassc <danielfreitassc@gmail.com>
2024-10-06 02:47:28 -0300

    Feat: Adicionando link de projetos

--- frontend/src/core/constants/Projetos.ts
+++ frontend/src/core/constants/Projetos.ts
@@ -1,30 +1,32 @@
 import { IProjeto } from "../projeto/IProjeto";
-import Spring from "../../assets/Spring.jpg"
+import Arq from "../../assets/arquitetura.png"
+import Frotas from "../../assets/frotas.jpg"
+import Market from "../../assets/market.jpg"
 export const  Projeto:IProjeto = {
     projeto:[
         {    
             id:1,
-            image:Spring,
-            skill: "Java",
-            title:"PicPay",
-            description:"seila",
-            linkProject:"https://github.com/DanielFreitassc"
+            image:Frotas,
+            skill: "Java Springboot PostgreSQL",
+            title:"Solução Frotas",
+            description:"Gestão de Frotas permite gerenciar motoristas, veículos, reservas e multas",
+            linkProject:"https://github.com/DanielFreitassc/SolucaoFrotas"
         },
         {    
             id:2,
-            image:Spring,
-            skill: "Java",
-            title:"PicPay",
-            description:"seila",
-            linkProject:"https://github.com/DanielFreitassc"
+            image:Arq,
+            skill: "Java Springboot H2",
+            title:"Desafio UOL",
+            description:"Cadastre jogadores com codinomes de listas específicas....",
+            linkProject:"https://github.com/DanielFreitassc/desafio-uol"
         },
         {    
             id:3,
-            image:Spring,
-            skill: "Java",
-            title:"PicPay",
-            description:"seila",
-            linkProject:"https://github.com/DanielFreitassc"
+            image:Market,
+            skill: "Java Springboot H2",
+            title:"Marketplace",
+            description:"API de Produtos para marketplace permite CRUD completo de produtos..",
+            linkProject:"https://github.com/DanielFreitassc/Marketplace"
         }
     ]
     
